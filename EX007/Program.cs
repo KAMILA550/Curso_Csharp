e6963624a30cb7b3fa6c6f54bc0cf9eb5731ff7a
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-16 21:06:44 -0300

    autolização do exercico

---
+++ EX007/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.Design;
+
+namespace EX007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double nota_final = 70;
+            string resultado;
+
+            if (nota_final >= 60)
+            {
+                resultado = " aprovado";
+            }
+            else
+            {
+                resultado = "reprovado";
+            }
+
+
+
+
+            Console.WriteLine("o aluno foi {0}.", resultado);
+        }
+    }
+}
