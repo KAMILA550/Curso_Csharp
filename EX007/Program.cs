b6f190c6e49dbaf5e3de53ddeaa51900ccd9d2b5
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-16 22:09:16 -0300

    nota do bimestre

--- EX007/Program.cs
+++ EX007/Program.cs
@@ -6,12 +6,31 @@ namespace EX007
     {
         static void Main(string[] args)
         {
-            double nota_final = 100;
+            double nota_final, n1, n2, n3, n4;
             string resultado;
+
+            Console.Write("digite a primeira  nota final do aluno: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a segunda  nota final do aluno: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a terceira   nota final do aluno: ");
+            n3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("digite a quarta  nota final do aluno: ");
+            n4 = Convert.ToDouble(Console.ReadLine());
+
+            nota_final = (n1 + n2 + n3 + n4) / 4;
+
+
+
+
+
 
             if (nota_final >= 60)
             {
-                if (nota_final >= 90 )
+                if (nota_final >= 90)
                 {
                     resultado = " aprovado com louvor";
 
