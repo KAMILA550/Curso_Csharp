5470d939856ea9de8314feaa56300d21ce1f7923
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-23 20:46:49 -0300

    ex13

---
+++ ex13/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma, quant;
+
+            soma = quant = 0; 
+
+            for (int i = 0; i <= 500; i++)
+            {
+               if (i % 3 == 0)
+                {
+                    soma += i;
+                    quant++; 
+                }
+
+                
+               
+            } Console.WriteLine("A soma dos {0} valores informados é {1}. ", quant, soma);
+
+        }
+    }
+}
