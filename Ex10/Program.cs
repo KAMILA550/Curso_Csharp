779cbb4460a9cee4429b6ad244662a3e8517c512
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-23 19:33:42 -0300

    ex10

---
+++ Ex10/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            salario = float(input(" Digite o salário: "))
+        }
+        
+
+
+
+    }
+}
