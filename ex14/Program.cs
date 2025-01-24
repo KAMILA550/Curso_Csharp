44a2829ae668231126fe5094716f4e0dda90fac2
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-23 21:15:18 -0300

    ex14

---
+++ ex14/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ex14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLineint num = Convert.ToInt32(Console.ReadLine());
+" digite um numero: "); 
+            for (int i = 1; i <= 10; i++)
+            {
+              Console.WriteLine("{0} x {1} = {2}", num, i, num * i);
+
+            }
+    }
+}
