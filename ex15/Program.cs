98858f8b6a68daf030191b3d2c6bc9fd9de63ad4
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-23 21:36:22 -0300

    GENRO

---
+++ ex15/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string genro;
+
+            Console.WriteLine("Digite o gênero: ");
+            genro = Console.ReadLine().ToUpper();
+
+            while(genro != "F" && genro != "M")
+            {
+                Console.WriteLine("Digite o gênero: ");
+                genro = Console.ReadLine();
+            }
+            Console.WriteLine("bem vindos(a)");
+        } 
+
+    }
+}
