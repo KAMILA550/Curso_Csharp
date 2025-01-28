f01ef157d21ccba5220aab632f502632b4a24358
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-28 19:45:27 -0300

    senha

---
+++ EX20/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EX20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+
+            while (num < 5)
+            {
+                num++;
+                Console.WriteLine(" curso de C#");
+            }
+        }   do 
+            {
+               num++; 
+              Console.writeline(" curso de C#");
+            } while (num < 5)
+    }
+}
---
+++ senha/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace senha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = "1234"; // senha correta
+            string senhaDigitada; // senha digitada pelo usuário
+            int tentativas = 0; // contador de tentativas
+
+            // loop para solicitar a senha ao usuário
+
+            do
+            {
+                Console.Write("Digite a senha: ");
+                senhaDigitada = Console.ReadLine();
+                tentativas++; // incrementa o contador de tentativas
+
+                // verifica se a senha digitada é diferente da senha correta 
+                if (senhaDigitada != senha && tentativas < 3)
+                {
+                    Console.WriteLine("Senha incorreta!!");
+                    Console.WriteLine(" você tem mais " + (3 - tentativas) + " tentativas"); 
+                    Console.ReadKey(); 
+                }
+                else if(senhaDigitada != senha && tentativas ==3 )
+                {
+                    Console.WriteLine("Senha incorreta! seu acesso foi bloqueado");
+                    return;
+                }
+            } while (senhaDigitada != senha);
+
+            Console.Clear();
+            Console.WriteLine("Senha correta! - Tentativas: " + tentativas );
+        }    
+}
+    }    
