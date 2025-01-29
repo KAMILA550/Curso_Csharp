13c7b2a914507dc488524b97081168034a0a1329
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-28 21:30:45 -0300

    VETOR ERRADO

---
+++ vetor/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            int[] num = new int[5];
+
+            num[0] = 10;
+            num[1] = 20;
+            num[2] = 30;
+            num[3] = 40;
+            num[4] = 50;
+            // num[5] = 60; Erro passou dos limites do vetor, vetor só vai até 4 posições
+
+            Console.WriteLine(num[2]);
+
+            int[] n = new int[5] { 60, 70, 80, 90, 100 };
+
+            Console.WriteLine(n[2]);
+
+            int[] numero = { 65, 75, 84, 76, 12, 8 }; 
+            Console.WriteLine(numero[3]);
+        }   */
+             /*
+            Random random = new Random();
+
+            int[] vetor = new int[10];
+
+            for (int i = 0; i < 10; i++) ;
+            {
+                vetor[i] = random.Next(1, 100);
+                Console.WriteLine(vetor[i]);
+            }
+        }
+
+        Console.writeline NewStruct;
+        double[] reais = new double[10]; 
+            
+             for (int i = 0; i< 10; I++)
+             {
+               reais[i] = random.NextDoubleNewStruct;
+               Console.WriteLine(reais[i]);
+             }
+              
+
