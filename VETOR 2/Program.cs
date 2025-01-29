619bbbe820085121314bb16d060068e2f5d966c0
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-28 21:39:47 -0300

    vetor errado

---
+++ VETOR 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.CompilerServices;
+
+namespace VETOR_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Maria", "João", "José", "Pedro", "Ana", "Carlos", "Antônio", "Francisco", "Paulo", "Luiz" };
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                if (i < nomes.Length - 1)
+                    Console.WriteLine(nomes[i] + ",");
+            }
+           else
+            {
+                Console.WriteLine(nomes[i] + ".");
+            }
+
+        }
+    }
+}
