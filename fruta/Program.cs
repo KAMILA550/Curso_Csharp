eeac64ce95c584cb8238d996398354963470ef92
kamila <kamila.garaujo@senacsp.edu.br>
2025-01-30 20:53:34 -0300

    metedos

---
+++ fruta/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace fruta
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "banana", "maçã", "laranja", "melancia", "abacaxi", "uva" };
+
+             for (int i = 0; i < nomes.Length; i++)
+            {
+                Console.WriteLine(nomes[i] + ",");
+
+            }
+
+              
+        }
+    }
+}
---
+++ metedos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.Serialization;
+
+namespace metedos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            saudacao("kamila"); // chamando o metodo saudaçao
+
+            soma(10, 20); // 
+        }
+
+        static void saudacao(string nome)
+        {
+            Console.WriteLine($"Olá{nome}, seja bem vindo!");
+        }
+
+        static void soma(int a, int b)
+        {
+            int resultado = a + b;
+            Console.WriteLine($"A soma de {a} + {b} é igual a {resultado}");
+            Console.WriteLine(" a soma de {0} + {1} é igual a {2}.", a, b, resultado);
+            Console.WriteLine("a soma de " + a + " + " + b + " é igual a " + resultado + ".");
+        }
+    }   }
